e7bc5027a8d8e4f2b6478a63bae89cce89bc0444
Andrej Ograjenšek <ograjensek.andrej@gmail.com>
2018-12-01 15:18:00 +0100

    Fixed tab order

--- src/Ogi.IntervalTimer/MainWindow.Designer.cs
+++ src/Ogi.IntervalTimer/MainWindow.Designer.cs
@@ -44,7 +44,7 @@
       this.button1.Location = new System.Drawing.Point(238, 5);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 21);
-      this.button1.TabIndex = 0;
+      this.button1.TabIndex = 3;
       this.button1.Text = "Start timer";
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -62,7 +62,7 @@
       this.progressBar1.Name = "progressBar1";
       this.progressBar1.Size = new System.Drawing.Size(385, 19);
       this.progressBar1.Step = 1;
-      this.progressBar1.TabIndex = 2;
+      this.progressBar1.TabIndex = 5;
       // 
       // label1
       // 
@@ -70,7 +70,7 @@
       this.label1.Location = new System.Drawing.Point(12, 9);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(113, 13);
-      this.label1.TabIndex = 3;
+      this.label1.TabIndex = 0;
       this.label1.Text = "Interval | Timeout [min]";
       // 
       // label2
@@ -79,7 +79,7 @@
       this.label2.Location = new System.Drawing.Point(177, 59);
       this.label2.Name = "label2";
       this.label2.Size = new System.Drawing.Size(53, 13);
-      this.label2.TabIndex = 3;
+      this.label2.TabIndex = 6;
       this.label2.Text = "00 m 00 s";
       // 
       // button2
@@ -87,7 +87,7 @@
       this.button2.Location = new System.Drawing.Point(317, 5);
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(80, 21);
-      this.button2.TabIndex = 0;
+      this.button2.TabIndex = 4;
       this.button2.Text = "Restart timer";
       this.button2.UseVisualStyleBackColor = true;
       this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -98,7 +98,7 @@
       this.checkBox1.Location = new System.Drawing.Point(344, 58);
       this.checkBox1.Name = "checkBox1";
       this.checkBox1.Size = new System.Drawing.Size(58, 17);
-      this.checkBox1.TabIndex = 4;
+      this.checkBox1.TabIndex = 7;
       this.checkBox1.Text = "On top";
       this.checkBox1.UseVisualStyleBackColor = true;
       this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
@@ -108,9 +108,9 @@
       this.textBox2.Location = new System.Drawing.Point(182, 6);
       this.textBox2.Name = "textBox2";
       this.textBox2.Size = new System.Drawing.Size(50, 20);
-      this.textBox2.TabIndex = 5;
+      this.textBox2.TabIndex = 2;
       // 
-      // form1
+      // MainWindow
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -124,7 +124,7 @@
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-      this.Name = "form1";
+      this.Name = "MainWindow";
       this.Text = "Timer";
       this.ResumeLayout(false);
       this.PerformLayout();
